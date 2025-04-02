66cb5831602541f46a2243b45efd0b0a475e2ef9
andredfc <andredfcarvalho@hotmail.com>
2025-04-02 11:22:49 -0300

    Criação do DB usando sqlite, com uma tabela User associada a classe User. Adição de métodos básicos de criação e consulta de User

---
+++ BackEnd/CantinasWebApi/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using CantinasWebApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Formats.Asn1;
+
+namespace CantinasWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<User>>> CreateUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetAllUsers()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var User = await _context.Users.FindAsync(id);
+
+            if(User == null)
+            {
+                return BadRequest("User does not exist.");
+            }
+            return Ok(User);
+        }
+    }
+}
