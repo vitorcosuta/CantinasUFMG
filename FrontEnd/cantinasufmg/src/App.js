3075d17346776bc1776e540ef9a8f0f880dff660
Vitor Costa <vitorcostamgbr@gmail.com>
2025-04-13 20:22:58 -0300

    adicionar estilo global de página

--- FrontEnd/cantinasufmg/src/App.js
+++ FrontEnd/cantinasufmg/src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 import AppRoutes from "./routes/AppRoutes";
+import { GlobalStyles } from "@mui/material";
 
 function App() {
-    return <AppRoutes />;
+    return (
+        <Fragment>
+            <GlobalStyles styles={{ "body, html": { margin: 0, padding: 0 } }} />
+            <AppRoutes />
+        </Fragment>
+    );
 }
 
 export default App;
