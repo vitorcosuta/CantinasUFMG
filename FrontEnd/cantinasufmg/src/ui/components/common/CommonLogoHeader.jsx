b5de3ab95b5d698feb5a0ad6ddbc44ec54515a05
Vitor Costa <vitorcostamgbr@gmail.com>
2025-04-13 19:58:29 -0300

    Incluir componentes comuns

---
+++ FrontEnd/cantinasufmg/src/ui/components/common/CommonLogoHeader.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Box from "@mui/material/Box";
+
+export const CommonLogoHeader = () => {
+
+    return (
+        <AppBar 
+            position="static"
+            sx={{ 
+                backgroundColor: '#EEEEEE',
+            }}
+        >
+            <Toolbar>
+                <Box component="img" src="/cantinas.png" alt="Logo" sx={{ height: 120, ml: '20vw' }} />
+            </Toolbar>
+        </AppBar>
+    );
+};
