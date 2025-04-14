8ef50116be439e6dd85f9e664bb8a873a4f35c76
andredfc <andredfcarvalho@hotmail.com>
2025-04-14 14:27:23 -0300

    Adicao de propriedade "Photo" em usuário

--- BackEnd/CantinasWebApi/Lanchonete.cs
+++ BackEnd/CantinasWebApi/Lanchonete.cs
@@ -3,7 +3,7 @@ namespace CantinasWebApi
     public class Lanchonete
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
+        public string Nome { get; set; } = string.Empty;
         public double posX { get; set; }
         public double posY { get; set; }
         public int idOwner { get; set; }
