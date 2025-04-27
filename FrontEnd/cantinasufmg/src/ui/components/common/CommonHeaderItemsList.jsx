d98997656f7a246c5e3635843345d49105823632
Pedro Henrique <pedrohfernandes2001@gmail.com>
2025-04-27 13:34:51 -0300

    Correção para item da header também estar selecionado em sub rotas

--- FrontEnd/cantinasufmg/src/ui/components/common/CommonHeaderItemsList.jsx
+++ FrontEnd/cantinasufmg/src/ui/components/common/CommonHeaderItemsList.jsx
@@ -9,39 +9,35 @@ export const CommonHeaderItemsList = () => {
 
     return (
         <>
-            {CommonHeaderItems.map((item) => (
-                <Button
-                    key={item.id}
-                    onClick={() => navigate(item.route)}
-                    startIcon={item.icon}
-                    sx={{
-                        color:
-                            location.pathname === item.route
-                                ? '#D84040'
-                                : '#1D1616',
-                        fontWeight:
-                            location.pathname === item.route
-                                ? 'bold'
-                                : 'normal',
-                        fontSize: '1rem',
-                        textTransform: 'none',
-                        border:
-                            location.pathname === item.route
+            {CommonHeaderItems.map((item) => {
+                const isActive = location.pathname.startsWith(item.route);
+
+                return (
+                    <Button
+                        key={item.id}
+                        onClick={() => navigate(item.route)}
+                        startIcon={item.icon}
+                        sx={{
+                            color: isActive ? '#D84040' : '#1D1616',
+                            fontWeight: isActive ? 'bold' : 'normal',
+                            fontSize: '1rem',
+                            textTransform: 'none',
+                            border: isActive
                                 ? '2px solid #D84040'
                                 : '2px solid transparent',
-                        borderRadius: '12px',
-                        backgroundColor:
-                            location.pathname === item.route
+                            borderRadius: '12px',
+                            backgroundColor: isActive
                                 ? '#FDECEC'
                                 : 'transparent',
-                        px: 2,
-                        py: 1,
-                        gap: 1,
-                    }}
-                >
-                    {item.label}
-                </Button>
-            ))}
+                            px: 2,
+                            py: 1,
+                            gap: 1,
+                        }}
+                    >
+                        {item.label}
+                    </Button>
+                );
+            })}
         </>
     );
 };
